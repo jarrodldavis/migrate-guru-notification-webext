a31586557a937781a0eca10ecb82f5484942d61d
Jarrod Davis <developer@jarrodldavis.com>
2019-02-18 00:54:39 -0700

    Update URL permissions
    - Narrow the page action and content script match pattern to only apply
      to individual Migrate Guru migration pages
    - Narrow the permissions to only include a match pattern permission for
      individual Migrate Guru migration pages
    - In Firefox, use the declarative `show_matches` key for the page action

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,9 +1,19 @@
+const MIGRATE_GURU_HOSTNAME = 'mg.blogvault.net';
+const MIGRATE_GURU_PATHNAME = '/migration/';
+
 browser.runtime.onInstalled.addListener((details) => {
   console.log('previousVersion', details.previousVersion)
 })
 
-browser.tabs.onUpdated.addListener(async (tabId) => {
-  browser.pageAction.show(tabId)
-})
+if (process.env.VENDOR !== 'firefox') {
+  browser.tabs.onUpdated.addListener(async (tabId, _change, tab) => {
+    const url = new URL(tab.url)
+    if (url.hostname === MIGRATE_GURU_HOSTNAME && url.pathname.startsWith(MIGRATE_GURU_PATHNAME)) {
+      browser.pageAction.show(tabId);
+    } else {
+      browser.pageAction.hide(tabId);
+    }
+  });
+}
 
 console.log(`'Allo 'Allo! Event Page for Page Action`)
